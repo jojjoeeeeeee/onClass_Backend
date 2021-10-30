4912935fcb716b3de67d80a2423c57a6d0533d3d
jojjoeeeeeee <jo-jo_kiki@hotmail.com>
2021-10-31 06:42:38 +0700

    exam std result submit

--- src/controllers/ExamController.js
+++ src/controllers/ExamController.js
@@ -1,6 +1,8 @@
 const Users = require('../models/user_schema');
 const Classes = require('../models/class_schema');
-const Exams = require('../models/exam/examination_schema.js');
+const Exams = require('../models/exam/examination_schema');
+const ExamResults = require('../models/exam/exam_result_schema');
+
 const moment = require('moment');
 
 exports.details = async (req,res) => {
@@ -15,7 +17,7 @@ exports.details = async (req,res) => {
         const class_data = await Classes.findOne({ class_code: classcode });
         if (!class_data) return res.status(404).json({result: 'Not found', message: ''});
         if (!class_data.teacher_id.includes(user_id) && !class_data.student_id.includes(user_id)) return res.status(403).json({result: 'Forbiden', message: 'You not in this class'});
-
+        
         if (!class_data.class_exam_id.includes(exam_id)) return res.status(404).json({result: 'Not found', message: ''});
 
         const exam_data = await Exams.findById(exam_id);
@@ -26,7 +28,16 @@ exports.details = async (req,res) => {
             const now = moment()
             const start = moment(exam_data.exam_start_date);
             const end = moment(exam_data.exam_end_date);
-            if (now.isBefore(start) || now.isAfter(end)) return res.status(403).json({result: 'Forbiden', message: 'Its not the time when you can take the exam.'})
+            if (now.isBefore(start) || now.isAfter(end)) return res.status(403).json({result: 'Forbiden', message: 'Its not the time when you can take the exam'});
+
+           //Already submit validate
+            const examResultData = await ExamResults.findOne({ exam_id : exam_id});
+            var already = false
+            examResultData.student_result.map((examKey) => {
+                if (examKey.student_id == user_id) return already = true
+            });
+            if (already) return res.status(403).json({result: 'Forbiden', message: 'You already submit this exam'});
+
 
             const res_part = []
             exam_data.part_list.map((key) => {
@@ -65,6 +76,68 @@ exports.details = async (req,res) => {
     
 };
 
+exports.stdSubmit = async (req,res) => {
+    const user_id = req.userId;
+    const classcode = req.body.class_code;
+    const exam_id = req.body.examId;
+    const submitResult = req.body.data;
+
+    //submit result validation
+    if (!classcode||!exam_id||!submitResult||submitResult.length == 0) return res.status(400).json({result: 'Bad request', message: ''});
+
+    var resultNotValid = false
+    submitResult.map((key) => {
+        if (!key.part_id||!key.part_type||!key.answer||key.answer.length == 0) return resultNotValid = true
+    });
+
+    if (resultNotValid) return res.status(400).json({result: 'Bad request', message: ''});
+
+    try {
+        const class_data = await Classes.findOne({ class_code: classcode });
+        if (!class_data) return res.status(404).json({result: 'Not found', message: ''});
+        if (!class_data.student_id.includes(user_id)) return res.status(403).json({result: 'Forbiden', message: 'access is denied'});
+        
+        if (!class_data.class_exam_id.includes(exam_id)) return res.status(404).json({result: 'Not found', message: ''});
+
+        const exam_data = await Exams.findById(exam_id);
+        if (!exam_data) return res.status(404).json({result: 'Not found', message: ''});
+
+        //Validate time
+        const now = moment()
+        const start = moment(exam_data.exam_start_date);
+        const end = moment(exam_data.exam_end_date);
+        if (now.isBefore(start) || now.isAfter(end)) return res.status(403).json({result: 'Forbiden', message: 'Its not the time when you can take the exam'});
+        
+        //Already submit validate
+        const examResultData = await ExamResults.findOne({ exam_id : exam_id});
+        var already = false
+        examResultData.student_result.map((examKey) => {
+            submitResult.map((resultKey) => {
+                if (examKey.student_id == user_id && examKey.part_id == resultKey.part_id) return already = true
+            });
+        });
+        if (already) return res.status(403).json({result: 'Forbiden', message: 'You already submit this exam'});
+
+        //Create result schema
+        submitResult.map( async (key,index) => {
+            const stdResultData = await ExamResults.findOne({ exam_id : exam_id});
+            const resultSchema = {
+                student_id: user_id,
+                part_id: key.part_id,
+                part_type: key.part_type,
+                answer: key.answer
+            }
+            stdResultData.student_result.push(resultSchema);
+            const stdResultSaveData = await ExamResults.findOneAndUpdate({ exam_id: exam_id}, stdResultData);
+        });
+        res.status(200).json({result: 'OK', message: 'success sumbit exam result'});
+
+    } catch (e) {
+        res.status(500).json({result: 'Internal Server Error', message: ''});
+    }
+
+};
+
 exports.create = async (req,res) => {
     const user_id = req.userId;
     const classcode = req.body.class_code;
@@ -73,18 +146,31 @@ exports.create = async (req,res) => {
     const exam = req.body.exam;
     if (!classcode||!exam) return res.status(400).json({result: 'Bad request', message: ''});
     if (!exam.exam_name||!exam.part_list||!exam.exam_start_date||!exam.exam_end_date) return res.status(400).json({result: 'Bad request', message: ''})
-    //part_list should have validation
+    //part_list should have validation || this method just create empty teacher edit add choice then edit the model later
     
     try {
         const class_data = await Classes.findOne({ class_code: classcode });
         if (!class_data) return res.status(404).json({result: 'Not found', message: ''});
         if (!class_data.teacher_id.includes(user_id)) return res.status(403).json({result: 'Forbiden', message: 'access is denied'});
         const data = await Exams.create(exam);
-        class_data.class_exam_id.push(data._id);
+        const examId = data._id
+        class_data.class_exam_id.push(examId);
         const new_class_data = await Classes.findOneAndUpdate({ class_code: classcode }, class_data);        
-
+        
+        const resultSchema = {
+            exam_id: examId,
+            class_code: classcode,
+            student_result: [],
+            student_score: [],
+        }
+        
+        const examResultData = await ExamResults.create(resultSchema);
         res.status(200).json({result: 'OK', message: 'success created exam'});
     } catch (e) {
         res.status(500).json({result: 'Internal Server Error', message: ''});
     }
+};
+
+exports.delete = async (req,res) => {
+
 };
--- src/routes/exam.js
+++ src/routes/exam.js
@@ -5,6 +5,8 @@ const jwt = require('../jwt');
 const ExamController = require('../controllers/ExamController');
 
 router.post('/details', jwt.verify, ExamController.details);
+router.patch('/submit', jwt.verify, ExamController.stdSubmit);
 router.post('/create', jwt.verify, ExamController.create);
+router.delete('/delete', jwt.verify, ExamController.delete);
 
 module.exports = router;

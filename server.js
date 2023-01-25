0267234d2821e0d85fa6ea6837ff086ce52c5ef3
p.jonganurak <p.jonganurak@accenture.com>
2023-01-26 02:10:11 +0700

    [SP3.1] fix preflightContinue

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.use(helmet());
 const corsOptions = {
   origin: 'http://ec2-54-255-229-73.ap-southeast-1.compute.amazonaws.com',
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  optionsSuccessStatus: 200,
+  preflightContinue: false,
+	optionsSuccessStatus: 204,
   exposedHeaders: "Authorization",
 };
 
